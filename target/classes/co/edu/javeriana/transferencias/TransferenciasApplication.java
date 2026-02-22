15cb7f23c63f16974d4abf372b7bc976a8a6ed86
Laura Juliana Garzón arias <garzonlj@javeriana.edu.co>
2026-02-22 18:21:17 -0500

    Simplify Spring Boot application configuration
    Removed exclusion of DataSourceAutoConfiguration from Spring Boot application.

--- target/classes/co/edu/javeriana/transferencias/TransferenciasApplication.java
+++ target/classes/co/edu/javeriana/transferencias/TransferenciasApplication.java
@@ -2,11 +2,8 @@ package co.edu.javeriana.transferencias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
-})
+@SpringBootApplication
 public class TransferenciasApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferenciasApplication.class, args);
