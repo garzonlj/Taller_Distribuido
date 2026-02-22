ff6d30fc89938ea40c6525044f7780f05416a041
Laura Juliana Garzón arias <garzonlj@javeriana.edu.co>
2026-02-22 12:01:41 -0500

    EstadoTransferencia

---
+++ src/main/java/co/edu/javeriana/transferencias/model/EstadoTransferencia.java
@@ -0,0 +1,10 @@
+package com.universidad.transferencias.model;
+
+public enum EstadoTransferencia {
+    INICIADA,
+    DEBITO_COMPLETADO,
+    CREDITO_COMPLETADO,
+    COMPLETADA,
+    FALLIDA,
+    REVERTIDA
+}
